ecbb624074b5cf1d3c7b84a9e09aeb04c05744f4
MinuteMirth <fuzzymuzzy@gmail.com>
2017-08-12 20:22:09 +0300

    Added a lot of stuff

---
+++ fake-burberry-frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import "./styles/normalize.css";
+import "./styles/flexboxgrid2.css";
+import "./styles/styles.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
