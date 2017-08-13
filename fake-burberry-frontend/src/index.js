b88487a73a768d747d05fa63ce714c1c4934937b
MinuteMirth <fuzzymuzzy@gmail.com>
2017-08-13 08:26:15 +0300

    deleting files

--- fake-burberry-frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-import "./styles/normalize.css";
-import "./styles/flexboxgrid2.css";
-import "./styles/styles.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
